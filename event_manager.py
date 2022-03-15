84a6609757d6080f902f5e8ff4ed9fe89bb2a814
Sparky <59151395+Sparks29032@users.noreply.github.com>
2022-03-15 14:16:59 -0500

    Add files via upload

--- event_manager.py
+++ event_manager.py
@@ -12,27 +12,77 @@ def manage(character: Character):
 
             # keyboard events given key pressed
             if key.char == 'q':
-                character.keyboard_press(48, 38)
+                character.keyboard_press(46, 38)
             if key.char == 'w':
-                character.keyboard_press(43, 38)
+                character.keyboard_press(36, 38)
             if key.char == 'e':
-                character.keyboard_press(38, 38)
+                character.keyboard_press(26, 38)
             if key.char == 'r':
-                character.keyboard_press(33, 38)
+                character.keyboard_press(16, 38)
+            if key.char == 't':
+                character.keyboard_press(6, 38)
+            if key.char == 'y':
+                character.keyboard_press(-4, 38)
+            if key.char == 'u':
+                character.keyboard_press(-14, 38)
+            if key.char == 'i':
+                character.keyboard_press(-24, 38)
+            if key.char == 'o':
+                character.keyboard_press(-34, 38)
+            if key.char == 'p':
+                character.keyboard_press(-44, 38)
+            if key.char == 'a':
+                character.keyboard_press(42, 31)
+            if key.char == 's':
+                character.keyboard_press(32, 31)
+            if key.char == 'd':
+                character.keyboard_press(22, 31)
+            if key.char == 'f':
+                character.keyboard_press(12, 31)
             if key.char == 'g':
-                character.keyboard_press(26, 31)
+                character.keyboard_press(2, 31)
+            if key.char == 'h':
+                character.keyboard_press(-8, 31)
+            if key.char == 'j':
+                character.keyboard_press(-18, 31)
+            if key.char == 'k':
+                character.keyboard_press(-28, 31)
+            if key.char == 'l':
+                character.keyboard_press(-38, 31)
             if key.char == 'z':
-                character.keyboard_press(44, 23)
+                character.keyboard_press(38, 23)
             if key.char == 'x':
-                character.keyboard_press(39, 23)
+                character.keyboard_press(28, 23)
+            if key.char == 'c':
+                character.keyboard_press(18, 23)
+            if key.char == 'v':
+                character.keyboard_press(8, 23)
+            if key.char == 'b':
+                character.keyboard_press(-2, 23)
+            if key.char == 'n':
+                character.keyboard_press(-12, 23)
+            if key.char == 'm':
+                character.keyboard_press(-22, 23)
             if key.char == '1':
                 character.keyboard_press(50, 45)
             if key.char == '2':
-                character.keyboard_press(45, 45)
-            if key.char == '3':
                 character.keyboard_press(40, 45)
+            if key.char == '3':
+                character.keyboard_press(30, 45)
             if key.char == '4':
-                character.keyboard_press(35, 45)
+                character.keyboard_press(20, 45)
+            if key.char == '5':
+                character.keyboard_press(10, 45)
+            if key.char == '6':
+                character.keyboard_press(0, 45)
+            if key.char == '7':
+                character.keyboard_press(-10, 45)
+            if key.char == '8':
+                character.keyboard_press(-20, 45)
+            if key.char == '9':
+                character.keyboard_press(-30, 45)
+            if key.char == '0':
+                character.keyboard_press(-40, 45)
 
         # handles non-alphanumeric key presses
         except AttributeError:
