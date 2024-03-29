cb1b3329f36d31773fba5da9fb6b7eab2fd9cc53
Sparks29032 <Sparks29032@gmail.com>
2024-03-29 13:51:12 -0400

    Add release configs

---
+++ release_config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+GH_RELEASE_NOTES="""See [README.md](README.md) for detailed release notes.
+
+The release is also available at [PyPI](https://pypi.org/project/Release-Playground/).
+"""
