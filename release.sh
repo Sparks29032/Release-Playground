fd704f9fca21bb9020eaacf67e290a50e2636a5c
Sparks29032 <Sparks29032@gmail.com>
2024-04-30 16:49:58 -0400

    Fix

--- release.sh
+++ release.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
-../bg-release-scripts/basic_release.py $1 $2 \
+../bg-release-scripts/release.py $1 $2 \
 	--tag \
 	--github \
-	--gh-title "title $1" \
-	--gh-notes """See [CHANGELOG.rst](CHANGELOG.rst) for detailed release notes.
-
-The release is also available at [PyPI](https://pypi.org/project/diffpy.utils/) and [Conda](https://anaconda.org/conda-forge/diffpy.utils).
-"""\
+	--gh-title "Release $2" \
 	--pypi
