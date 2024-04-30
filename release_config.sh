ecc3d9abb94f68d1df729bcd3b9182da9049a480
Sparks29032 <Sparks29032@gmail.com>
2024-04-30 16:56:38 -0400

    update

--- release_config.sh
+++ release_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GH_RELEASE_NOTES="""See [README.md](README.md) for detailed release notes.
+GH_RELEASE_NOTES="""See [CHANGELOG.rst](CHANGELOG.rst) for detailed release notes.
 
-The release is also available at [PyPI](https://pypi.org/project/Release-Playground/).
+The release is also available at [PyPI](https://pypi.org/project/diffpy.utils/) and [Conda](https://anaconda.org/conda-forge/diffpy.utils).
 """
